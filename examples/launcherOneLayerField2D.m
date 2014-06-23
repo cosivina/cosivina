932cd005683665676364bd531377bb31aa13472d
Sebastian Schneegans <sebastian.schneegans@ini.rub.de>
2014-06-23 14:12:58 +0200

    cleaned up and standardized examples for use in DFT book exercises

--- examples/launcherOneLayerField2D.m
+++ examples/launcherOneLayerField2D.m
@@ -10,7 +10,7 @@
 % shared parameters
 fieldSize = 100;
 sigma_exc = 5;
-sigma_inh = 12.5;
+sigma_inh = 10;
 
 % create simulator object
 sim = Simulator();
@@ -30,26 +30,21 @@ sim.addElement(NeuralField('field u', [fieldSize, fieldSize], 20, -5, 4), ...
   {'stimulus 1', 'stimulus 2', 'stimulus 3', 'stimulus 4'});
 
 % create interactions
-sim.addElement(MexicanHatKernel2D('u -> u', [fieldSize, fieldSize], sigma_exc, sigma_exc, 0, ...
-  sigma_inh, sigma_inh, 0), 'field u', [], 'field u');
-% sim.addElement(GaussKernel2D('u -> u (sharp)', [fieldSize, fieldSize], sigma_exc, sigma_exc, 0), ...
-%   'field u', [], 'field u');
-% sim.addElement(GaussKernel2D('u -> u (broad)', [fieldSize, fieldSize], sigma_inh, sigma_inh, 0), ...
-%   'field u', [], 'field u');
-sim.addElement(SumDimension('u -> u (global)', [1, 2], [1, 1], 0), 'field u', [], 'field u');
+sim.addElement(LateralInteractions2D('u -> u', [fieldSize, fieldSize], sigma_exc, sigma_exc, 0, ...
+  sigma_inh, sigma_inh, 0, 0), 'field u', [], 'field u');
 
 % create noise stimulus and noise kernel
 sim.addElement(NormalNoise('noise', [fieldSize, fieldSize], 1.0));
-sim.addElement(GaussKernel2D('noise kernel', [fieldSize, fieldSize], 0, 0, 0), 'noise', [], 'field u');
+sim.addElement(GaussKernel2D('noise kernel', [fieldSize, fieldSize], 0, 0, 1.0), 'noise', [], 'field u');
 
 
 %% setting up the GUI
 
-elementGroups = {'field u', 'u -> u', 'u -> u (global)', ...
-  'stimulus 1', 'stimulus 2', 'stimulus 3', 'stimulus 4', 'noise kernel'};
+elementGroupLabels = {'field u', 'kernel u -> u', 'stimulus 1', 'stimulus 2', 'stimulus 3', 'stimulus 4', 'noise kernel'};
+elementGroups = {'field u', 'u -> u', 'stimulus 1', 'stimulus 2', 'stimulus 3', 'stimulus 4', 'noise kernel'};
 
 gui = StandardGUI(sim, [50, 50, 1000, 600], 0.01, [0.0, 0.3, 1.0, 0.7], [1, 2], 0.075, ...
-  [0.0, 0.0, 1.0, 0.3], [6, 4], elementGroups, elementGroups);
+  [0.0, 0.0, 1.0, 0.3], [6, 4], elementGroupLabels, elementGroups);
 
 gui.addVisualization(ScaledImage('field u', 'activation', [-10, 10], {}, {}, 'field u activation'), [1, 1]);
 gui.addVisualization(ScaledImage('field u', 'output', [0, 1], {}, {}, 'field u output'), [1, 2]);
@@ -59,39 +54,39 @@ gui.addVisualization(ScaledImage('field u', 'output', [0, 1], {}, {}, 'field u o
 gui.addControl(ParameterSlider('h', 'field u', 'h', [-10, 0], '%0.1f', 1, 'resting level of field u'), [1, 1]);
 gui.addControl(ParameterSlider('q', 'noise kernel', 'amplitude', [0, 10], '%0.1f', 1, ...
   'noise level for field u'), [1, 3]);
-% lateral interactions
-gui.addControl(ParameterSlider('c_exc', 'u -> u', 'amplitudeExc', [0, 40], '%0.1f', 1, ...
-  'strength of lateral excitation'), [2, 1]);
-gui.addControl(ParameterSlider('c_inh', 'u -> u', 'amplitudeInh', [0, 40], '%0.1f', 1, ...
-  'strength of lateral inhibition'), [2, 2]);
-gui.addControl(ParameterSlider('c_gi', 'u -> u (global)', 'amplitude', [0, 0.25], '%0.2f', -1, ...
-  'strength of global inhibition'), [2, 3]);
 
+% lateral interactions
+gui.addControl(ParameterSlider('c_exc', 'u -> u', 'amplitudeExc', [0, 50], '%0.1f', 1, ...
+  'strength of lateral excitation'), [2, 1]);
+gui.addControl(ParameterSlider('c_inh', 'u -> u', 'amplitudeInh', [0, 50], '%0.1f', 1, ...
+  'strength of lateral inhibition'), [2, 2]);
+gui.addControl(ParameterSlider('c_glob', 'u -> u', 'amplitudeGlobal', [0, 0.25], '%0.3f', -1, ...
+  'strength of global inhibition'), [2, 3]);
 
 % stimuli
 gui.addControl(ParameterSlider('px_s1', 'stimulus 1', 'positionX', [0, fieldSize], '%0.1f', 1, ...
   'horizontal position of stimulus 1'), [3, 1]);
 gui.addControl(ParameterSlider('py_s1', 'stimulus 1', 'positionY', [0, fieldSize], '%0.1f', 1, ...
   'vertical position of stimulus 1'), [3, 2]);
-gui.addControl(ParameterSlider('c_s1', 'stimulus 1', 'amplitude', [0, 20], '%0.1f', 1, ...
-  'stength of stimulus 1'), [3, 3]);
+gui.addControl(ParameterSlider('a_s1', 'stimulus 1', 'amplitude', [0, 20], '%0.1f', 1, ...
+  'amplitude of stimulus 1'), [3, 3]);
 
 gui.addControl(ParameterSlider('px_s2', 'stimulus 2', 'positionX', [0, fieldSize], '%0.1f', 1, ...
   'horizontal position of stimulus 2'), [4, 1]);
 gui.addControl(ParameterSlider('py_s2', 'stimulus 2', 'positionY', [0, fieldSize], '%0.1f', 1, ...
   'vertical position of stimulus 2'), [4, 2]);
-gui.addControl(ParameterSlider('c_s2', 'stimulus 2', 'amplitude', [0, 20], '%0.1f', 1, ...
-  'stength of stimulus 2'), [4, 3]);
+gui.addControl(ParameterSlider('a_s2', 'stimulus 2', 'amplitude', [0, 20], '%0.1f', 1, ...
+  'amplitude of stimulus 2'), [4, 3]);
 
 gui.addControl(ParameterSlider('py_s3', 'stimulus 3', 'positionY', [0, fieldSize], '%0.1f', 1, ...
   'vertical position of stimulus 3'), [5, 2]);
-gui.addControl(ParameterSlider('c_s3', 'stimulus 3', 'amplitude', [0, 20], '%0.1f', 1, ...
-  'stength of stimulus 3'), [5, 3]);
+gui.addControl(ParameterSlider('a_s3', 'stimulus 3', 'amplitude', [0, 20], '%0.1f', 1, ...
+  'amplitude of stimulus 3'), [5, 3]);
 
 gui.addControl(ParameterSlider('px_s4', 'stimulus 4', 'positionX', [0, fieldSize], '%0.1f', 1, ...
   'horizontal position of stimulus 4'), [6, 1]);
-gui.addControl(ParameterSlider('c_s4', 'stimulus 4', 'amplitude', [0, 20], '%0.1f', 1, ...
-  'stength of stimulus 4'), [6, 3]);
+gui.addControl(ParameterSlider('a_s4', 'stimulus 4', 'amplitude', [0, 20], '%0.1f', 1, ...
+  'amplitude of stimulus 4'), [6, 3]);
 
 % add buttons
 gui.addControl(GlobalControlButton('Pause', gui, 'pauseSimulation', true, false, false, 'pause simulation'), [1, 4]);
--- examples/launcherTransformation.m
+++ examples/launcherTransformation.m
@@ -13,7 +13,7 @@
 % shared parameters
 fieldRange = 60;
 sigma_exc = 5;
-sigma_inh = 12.5;
+sigma_inh = 10;
 
 fieldSize = 2 * fieldRange + 1;
 
@@ -21,76 +21,80 @@ fieldSize = 2 * fieldRange + 1;
 sim = Simulator();
 
 % create inputs
-sim.addElement(GaussStimulus1D('stim_r1', fieldSize, sigma_exc, 0, fieldRange + 1 - fieldRange/2, 0, 0));
-sim.addElement(GaussStimulus1D('stim_r2', fieldSize, sigma_exc, 0, fieldRange + 1 + fieldRange/2, 0, 0));
-sim.addElement(GaussStimulus1D('stim_g', fieldSize, sigma_exc, 0, fieldRange + 1, 0, 0));
-sim.addElement(GaussStimulus1D('stim_b1', 2*fieldSize-1, sigma_exc, 0, fieldSize - fieldRange/2, 0, 0));
-sim.addElement(GaussStimulus1D('stim_b2', 2*fieldSize-1, sigma_exc, 0, fieldSize + fieldRange/2, 0, 0));
+sim.addElement(GaussStimulus1D('stimulus r1', fieldSize, sigma_exc, 0, fieldRange + 1 - fieldRange/2, 0, 0));
+sim.addElement(GaussStimulus1D('stimulus r2', fieldSize, sigma_exc, 0, fieldRange + 1 + fieldRange/2, 0, 0));
+sim.addElement(GaussStimulus1D('stimulus g', fieldSize, sigma_exc, 0, fieldRange + 1, 0, 0));
+sim.addElement(GaussStimulus1D('stimulus b1', 2*fieldSize-1, sigma_exc, 0, fieldSize - fieldRange/2, 0, 0));
+sim.addElement(GaussStimulus1D('stimulus b2', 2*fieldSize-1, sigma_exc, 0, fieldSize + fieldRange/2, 0, 0));
 
-sim.addElement(BoostStimulus('boost_r', 0));
-sim.addElement(BoostStimulus('boost_g', 0));
-sim.addElement(BoostStimulus('boost_b', 0));
-sim.addElement(BoostStimulus('boost_t', 0));
+sim.addElement(BoostStimulus('boost r', 0));
+sim.addElement(BoostStimulus('boost g', 0));
+sim.addElement(BoostStimulus('boost b', 0));
+sim.addElement(BoostStimulus('boost t', 0));
 
 % create neural field
-sim.addElement(NeuralField('r', fieldSize, 20, -5, 4), {'stim_r1', 'stim_r2', 'boost_r'});
-sim.addElement(NeuralField('g', fieldSize, 20, -5, 4), {'stim_g', 'boost_g'});
-sim.addElement(NeuralField('b', 2*fieldSize-1, 20, -5, 4), {'stim_b1', 'stim_b2', 'boost_b'});
-sim.addElement(NeuralField('t', [fieldSize, fieldSize], 20, -5, 4), {'boost_t'});
+sim.addElement(NeuralField('field r', fieldSize, 20, -5, 4), {'stimulus r1', 'stimulus r2', 'boost r'});
+sim.addElement(NeuralField('field g', fieldSize, 20, -5, 4), {'stimulus g', 'boost g'});
+sim.addElement(NeuralField('field b', 2*fieldSize-1, 20, -5, 4), {'stimulus b1', 'stimulus b2', 'boost b'});
+sim.addElement(NeuralField('field t', [fieldSize, fieldSize], 20, -5, 4), {'boost t'});
 
 % create interactions
 
 % lateral interactions for field t
-sim.addElement(GaussKernel2D('tt_exc', [fieldSize, fieldSize], sigma_exc, sigma_exc, 0, false, false, true), 't', 'output', 't');
-sim.addElement(GaussKernel2D('tt_inh', [fieldSize, fieldSize], sigma_inh, sigma_inh, 0, false, false, true), 't', 'output', 't');
-sim.addElement(SumAllDimensions('sum_t', [fieldSize, fieldSize]), 't', 'output');
-sim.addElement(DiagonalSum('diag_t', [fieldSize, fieldSize], 1), 't', 'output');
-sim.addElement(ScaleInput('tt_global', 1, 0.0), 'sum_t', 'fullSum', 't');
+sim.addElement(LateralInteractions2D('t -> t', [fieldSize, fieldSize], sigma_exc, sigma_exc, 0, sigma_inh, sigma_inh, 0, ...
+  0, false, false, true), 'field t', 'output', 'field t');
+sim.addElement(SumAllDimensions('sum t', [fieldSize, fieldSize]), 'field t', 'output');
+sim.addElement(DiagonalSum('diag t', [fieldSize, fieldSize], 1), 'field t', 'output');
 
 % lateral interactions in fields r, g, and b
-sim.addElement(LateralInteractions1D('rr', fieldSize, sigma_exc, 0, sigma_inh, 0, 0, false, true), 'r', 'output', 'r');
-sim.addElement(LateralInteractions1D('gg', fieldSize, sigma_exc, 0, sigma_inh, 0, 0, false, true), 'g', 'output', 'g');
-sim.addElement(LateralInteractions1D('bb', 2*fieldSize-1, sigma_exc, 0, sigma_inh, 0, 0, false, true), 'b', 'output', 'b');
+sim.addElement(LateralInteractions1D('r -> r', fieldSize, sigma_exc, 0, sigma_inh, 0, 0, false, true), 'field r', 'output', 'field r');
+sim.addElement(LateralInteractions1D('g -> g', fieldSize, sigma_exc, 0, sigma_inh, 0, 0, false, true), 'field g', 'output', 'field g');
+sim.addElement(LateralInteractions1D('b -> b', 2*fieldSize-1, sigma_exc, 0, sigma_inh, 0, 0, false, true), 'field b', 'output', 'field b');
 
 % interactions between fields
-sim.addElement(GaussKernel1D('tr', fieldSize, sigma_exc, 0, false, true), 'r', 'output');
-sim.addElement(ExpandDimension2D('exp_tr', 1, [fieldSize, fieldSize]), 'tr', 'output', 't');
-sim.addElement(GaussKernel1D('tg', fieldSize, sigma_exc, 0, false, true), 'g', 'output');
-sim.addElement(ExpandDimension2D('exp_tg', 2, [fieldSize, fieldSize]), 'tg', 'output', 't');
-sim.addElement(GaussKernel1D('tb', 2*fieldSize-1, sigma_exc, 0, false, true), 'b', 'output');
-sim.addElement(DiagonalExpansion('exp_tb', 2*fieldSize-1, 0), 'tb', 'output', 't');
+sim.addElement(GaussKernel1D('r -> t', fieldSize, sigma_exc, 0, false, true), 'field r', 'output');
+sim.addElement(ExpandDimension2D('expand r -> t', 1, [fieldSize, fieldSize]), 'r -> t', 'output', 'field t');
+sim.addElement(GaussKernel1D('g -> t', fieldSize, sigma_exc, 0, false, true), 'field g', 'output');
+sim.addElement(ExpandDimension2D('expand g -> t', 2, [fieldSize, fieldSize]), 'g -> t', 'output', 'field t');
+sim.addElement(GaussKernel1D('b -> t', 2*fieldSize-1, sigma_exc, 0, false, true), 'field b', 'output');
+sim.addElement(DiagonalExpansion('expand b -> t', 2*fieldSize-1, 0), 'b -> t', 'output', 'field t');
 
-sim.addElement(GaussKernel1D('rt', fieldSize, sigma_exc, 0, false, true), 'sum_t', 'verticalSum', 'r');
-sim.addElement(GaussKernel1D('gt', fieldSize, sigma_exc, 0, false, true), 'sum_t', 'horizontalSum', 'g');
-sim.addElement(GaussKernel1D('bt', 2*fieldSize-1, sigma_exc, 0, false, true), 'diag_t', 'output', 'b');
+sim.addElement(GaussKernel1D('t -> r', fieldSize, sigma_exc, 0, false, true), 'sum t', 'verticalSum', 'field r');
+sim.addElement(GaussKernel1D('t -> g', fieldSize, sigma_exc, 0, false, true), 'sum t', 'horizontalSum', 'field g');
+sim.addElement(GaussKernel1D('t -> b', 2*fieldSize-1, sigma_exc, 0, false, true), 'diag t', 'output', 'field b');
 
+sim.addElement(NormalNoise('noise r', [1, fieldSize], 1.0), [], [], 'field r');
+sim.addElement(NormalNoise('noise g', [1, fieldSize], 1.0), [], [], 'field g');
+sim.addElement(NormalNoise('noise b', [1, 2*fieldSize-1], 1.0), [], [], 'field b');
+sim.addElement(NormalNoise('noise t', [fieldSize, fieldSize], 1.0), [], [], 'field t');
 
 %% setting up the GUI
 
+elementGroupLabels = {'field r', 'field g', 'field b', 'field t', ...
+  'lateral kernel r -> r', 'lateral kernel g -> g', 'lateral kernel b -> b', 'lateral kernel t -> t', ...
+  'kernel r -> t', 'kernel g -> t', 'kernel b -> t', 'kernel t -> r', 'kernel t -> g', 'kernel t -> b', ...
+  'stimulus r1', 'stimulus r2', 'stimulus g', 'stimulus b1', 'stimulus b2', 'boost r', 'boost g', 'boost b', 'boost t', ...
+  'noise r', 'noise g', 'noise b', 'noise t'};
 elementGroups = {'field r', 'field g', 'field b', 'field t', ...
-  'r -> r', 'g -> g', 'b -> b', 't -> t (narrow)', 't -> t (wide)', 't -> t (global)', ...
+  'r -> r', 'g -> g', 'b -> b', 't -> t', ...
   'r -> t', 'g -> t', 'b -> t', 't -> r', 't -> g', 't -> b', ...
-  'stimulus r1', 'stimulus r2', 'stimulus g', 'stimulus b1', 'stimulus b2', 'boost r', 'boost g', 'boost b', 'boost t'};
-elementsInGroup = {'r', 'g', 'b', 't', ...
-  'rr', 'gg', 'bb', 'tt_exc', 'tt_inh', 'tt_global', ...
-  'tr', 'tg', 'tb', 'rt', 'gt', 'bt', ...
-  'stim_r1', 'stim_r2', 'stim_g', 'stim_b1', 'stim_b2', 'boost_r', 'boost_g', 'boost_b', 'boost_t'};
+  'stimulus r1', 'stimulus r2', 'stimulus g', 'stimulus b1', 'stimulus b2', 'boost r', 'boost g', 'boost b', 'boost t', ...
+  'noise r', 'noise g', 'noise b', 'noise t'};
 
 figureWidth = 900;
-figureHeight = 4/5 * figureWidth;
+figureHeight = 3/4 * figureWidth;
 S = diag([figureHeight/figureWidth, 1, figureHeight/figureWidth, 1]);
 
 % create GUI object
-gui = StandardGUI(sim, [50, 50, figureWidth, figureHeight], 0.01, [0.0, 0.0, 0.8, 1.0], [1, 1], 0.05, ...
-  [0.8, 0.0, 0.2, 1.0], [25, 1], elementGroups, elementsInGroup);
-
+gui = StandardGUI(sim, [50, 50, figureWidth, figureHeight], 0.01, [0.0, 0.0, 0.75, 1.0], [1, 1], 0.05, ...
+  [0.75, 0.0, 0.25, 1.0], [28, 1], elementGroupLabels, elementGroups);
 
 w = 0.4; h = 0.125; m = 0.05;
-gui.addVisualization(ScaledImage('t', 'activation', [-7.5, 7.5], ...
+gui.addVisualization(ScaledImage('field t', 'activation', [-7.5, 7.5], ...
   {'YAxisLocation', 'right', 'YDir', 'normal', 'XTick', -40:20:40, 'YTick', -40:20:40}, ...
   {'XData', -fieldRange:fieldRange, 'YData', -fieldRange:fieldRange}, ...
   '', 'retinal position', 'gaze direction', [h+2*m, 1-w-h-2*m, w, w] * S));
-gui.addVisualization(MultiPlot({'r', 'r', 'stim_r1', 'stim_r2'}, {'activation', 'output', 'output', 'output'}, ...
+gui.addVisualization(MultiPlot({'field r', 'field r', 'stimulus r1', 'stimulus r2'}, {'activation', 'output', 'output', 'output'}, ...
   [1, 10, 1, 1], 'horizontal', ...
   {'YLim', [-10, 10], 'XLim', [-fieldRange, fieldRange], 'Box', 'on', 'YGrid', 'on', 'XTick', -40:20:40}, ...
   {{'Color', 'b', 'LineWidth', 2, 'XDataMode', 'manual', 'XData', -fieldRange:fieldRange}, ...
@@ -98,8 +102,8 @@ gui.addVisualization(MultiPlot({'r', 'r', 'stim_r1', 'stim_r2'}, {'activation', 
   {'Color', [0, 0.75, 0], 'XDataMode', 'manual', 'XData', -fieldRange:fieldRange}, ...
   {'Color', [0, 0.75, 0], 'XDataMode', 'manual', 'XData', -fieldRange:fieldRange}}, ...
   'retinal field', '', 'activation', [h+2*m, 1-h-m, w, h] * S));
-gui.addVisualization(MultiPlot({'g', 'g', 'stim_g'}, {'activation', 'output', 'output'}, ...
-  [1, 10, 1, 1], 'vertical', ...
+gui.addVisualization(MultiPlot({'field g', 'field g', 'stimulus g'}, {'activation', 'output', 'output'}, ...
+  [1, 10, 1], 'vertical', ...
   {'XLim', [-10, 10], 'XDir', 'reverse', 'YLim', [-fieldRange, fieldRange], 'YAxisLocation', 'right', 'Box', 'on', ...
   'XGrid', 'on', 'YTick', -40:20:40}, ...
   {{'Color', 'b', 'LineWidth', 2, 'XDataMode', 'manual', 'YData', -fieldRange:fieldRange}, ...
@@ -113,7 +117,7 @@ d = h/sqrt(2) + w; % size of diagonal axes
 up = [(2*fieldSize-1)/(w*sqrt(2))-2, diff(ylim_b)/h, 0]; % "up vector" for diagonal axes
 up = up/norm(up, 2);
 pbar = [sqrt(2)*w, h, 1]; % "plot box aspect ratio" for diagonal axes
-gui.addVisualization(MultiPlot({'b', 'b', 'stim_b1', 'stim_b2'}, {'activation', 'output', 'output', 'output'}, ...
+gui.addVisualization(MultiPlot({'field b', 'field b', 'stimulus b1', 'stimulus b2'}, {'activation', 'output', 'output', 'output'}, ...
   [1, 10, 1, 1], 'horizontal', ...
   {'XLim', [-fieldSize+1, fieldSize-1], 'YLim', ylim_b, 'PlotBoxAspectRatio', pbar, 'CameraUpVector', up, 'Box', 'on', ...
   'XTick', -120:20:120, 'YGrid', 'on'}, ...
@@ -125,32 +129,36 @@ gui.addVisualization(MultiPlot({'b', 'b', 'stim_b1', 'stim_b2'}, {'activation', 
 
 
 % add sliders
-gui.addControl(ParameterSlider('p_r1', 'stim_r1', 'position', [1, fieldSize], '%0.1f', 1, 'position of retinal stimulus 1'), [1, 1]);
-gui.addControl(ParameterSlider('c_r1', 'stim_r1', 'amplitude', [0, 10], '%0.1f', 1, 'strength of retinal stimulus 1'), [2, 1]);
-gui.addControl(ParameterSlider('p_r2', 'stim_r2', 'position', [1, fieldSize], '%0.1f', 1, 'position of retinal stimulus 2'), [3, 1]);
-gui.addControl(ParameterSlider('c_r2', 'stim_r2', 'amplitude', [0, 10], '%0.1f', 1, 'strength of retinal stimulus 2'), [4, 1]);
-gui.addControl(ParameterSlider('bst_r', 'boost_r', 'amplitude', [0, 10], '%0.1f', 1, 'boost of retinal field'), [5, 1]);
+gui.addVisualization(StaticText('stimuli for field r'), [1, 1], [1, 1], 'control');
+gui.addControl(ParameterSlider('p_s1', 'stimulus r1', 'position', [1, fieldSize], '%0.1f', 1, 'position of retinal stimulus 1'), [2, 1]);
+gui.addControl(ParameterSlider('a_s1', 'stimulus r1', 'amplitude', [0, 10], '%0.1f', 1, 'amplitude of retinal stimulus 1'), [3, 1]);
+gui.addControl(ParameterSlider('p_s2', 'stimulus r2', 'position', [1, fieldSize], '%0.1f', 1, 'position of retinal stimulus 2'), [4, 1]);
+gui.addControl(ParameterSlider('a_s2', 'stimulus r2', 'amplitude', [0, 10], '%0.1f', 1, 'amplitude of retinal stimulus 2'), [5, 1]);
+gui.addControl(ParameterSlider('boost', 'boost r', 'amplitude', [0, 10], '%0.1f', 1, 'boost of retinal field'), [6, 1]);
 
-gui.addControl(ParameterSlider('p_g', 'stim_g', 'position', [1, fieldSize], '%0.1f', 1, 'position of gaze field stimulus'), [7, 1]);
-gui.addControl(ParameterSlider('c_g', 'stim_g', 'amplitude', [0, 10], '%0.1f', 1, 'strength of gaze field stimulus'), [8, 1]);
-gui.addControl(ParameterSlider('bst_g', 'boost_g', 'amplitude', [0, 10], '%0.1f', 1, 'boost of gaze field'), [9, 1]);
+gui.addVisualization(StaticText('stimuli for field g'), [8, 1], [1, 1], 'control');
+gui.addControl(ParameterSlider('p_s1', 'stimulus g', 'position', [1, fieldSize], '%0.1f', 1, 'position of gaze field stimulus'), [9, 1]);
+gui.addControl(ParameterSlider('a_s1', 'stimulus g', 'amplitude', [0, 10], '%0.1f', 1, 'strength of gaze field stimulus'), [10, 1]);
+gui.addControl(ParameterSlider('boost', 'boost g', 'amplitude', [0, 10], '%0.1f', 1, 'boost of gaze field'), [11, 1]);
 
-gui.addControl(ParameterSlider('p_b1', 'stim_b1', 'position', [1, 2*fieldSize-1], '%0.1f', 1, 'position of body-centered stimulus 1'), [11, 1]);
-gui.addControl(ParameterSlider('c_b1', 'stim_b1', 'amplitude', [0, 10], '%0.1f', 1, 'strength of body-centered stimulus 1'), [12, 1]);
-gui.addControl(ParameterSlider('p_b2', 'stim_b2', 'position', [1, 2*fieldSize-1], '%0.1f', 1, 'position of body-centered stimulus 2'), [13, 1]);
-gui.addControl(ParameterSlider('c_b2', 'stim_b2', 'amplitude', [0, 10], '%0.1f', 1, 'strength of body-centered stimulus 2'), [14, 1]);
-gui.addControl(ParameterSlider('bst_b', 'boost_b', 'amplitude', [0, 10], '%0.1f', 1, 'boost of body-centered field'), [15, 1]);
+gui.addVisualization(StaticText('stimuli for field b'), [13, 1], [1, 1], 'control');
+gui.addControl(ParameterSlider('p_s1', 'stimulus b1', 'position', [1, 2*fieldSize-1], '%0.1f', 1, 'position of body-centered stimulus 1'), [14, 1]);
+gui.addControl(ParameterSlider('a_s1', 'stimulus b1', 'amplitude', [0, 10], '%0.1f', 1, 'amplitude of body-centered stimulus 1'), [15, 1]);
+gui.addControl(ParameterSlider('p_s2', 'stimulus b2', 'position', [1, 2*fieldSize-1], '%0.1f', 1, 'position of body-centered stimulus 2'), [16, 1]);
+gui.addControl(ParameterSlider('a_s2', 'stimulus b2', 'amplitude', [0, 10], '%0.1f', 1, 'amplitude of body-centered stimulus 2'), [17, 1]);
+gui.addControl(ParameterSlider('boost', 'boost b', 'amplitude', [0, 10], '%0.1f', 1, 'boost of body-centered field'), [18, 1]);
 
-gui.addControl(ParameterSlider('bst_t', 'boost_t', 'amplitude', [0, 10], '%0.1f', 1, 'boost of transformation field'), [17, 1]);
+gui.addVisualization(StaticText('stimuli for field t'), [20, 1], [1, 1], 'control');
+gui.addControl(ParameterSlider('boost', 'boost t', 'amplitude', [0, 10], '%0.1f', 1, 'boost of transformation field'), [21, 1]);
 
 
 % add buttons
-gui.addControl(GlobalControlButton('Pause', gui, 'pauseSimulation', true, false, false, 'pause simulation'), [20, 1]);
-gui.addControl(GlobalControlButton('Reset', gui, 'resetSimulation', true, false, true, 'reset simulation'), [21, 1]);
-gui.addControl(GlobalControlButton('Parameters', gui, 'paramPanelRequest', true, false, false, 'open parameter panel'), [22, 1]);
-gui.addControl(GlobalControlButton('Save', gui, 'saveParameters', true, false, true, 'save parameter settings'), [23, 1]);
-gui.addControl(GlobalControlButton('Load', gui, 'loadParameters', true, false, true, 'load parameter settings'), [24, 1]);
-gui.addControl(GlobalControlButton('Quit', gui, 'quitSimulation', true, false, false, 'quit simulation'), [25, 1]);
+gui.addControl(GlobalControlButton('Pause', gui, 'pauseSimulation', true, false, false, 'pause simulation'), [23, 1]);
+gui.addControl(GlobalControlButton('Reset', gui, 'resetSimulation', true, false, true, 'reset simulation'), [24, 1]);
+gui.addControl(GlobalControlButton('Parameters', gui, 'paramPanelRequest', true, false, false, 'open parameter panel'), [25, 1]);
+gui.addControl(GlobalControlButton('Save', gui, 'saveParameters', true, false, true, 'save parameter settings'), [26, 1]);
+gui.addControl(GlobalControlButton('Load', gui, 'loadParameters', true, false, true, 'load parameter settings'), [27, 1]);
+gui.addControl(GlobalControlButton('Quit', gui, 'quitSimulation', true, false, false, 'quit simulation'), [28, 1]);
 
 %% run the simulator in the GUI
 
